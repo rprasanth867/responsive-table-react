0f7e0ae77c7144a3832f505810e2841e9fcb4312
prudrakshula <prudrakshula@nouveau-labs.com>
2023-11-25 07:25:08 +0530

    responsive table created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { personsData } from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className='table-container'>
+        <table>
+          <caption>This is the sample table data</caption>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Married</th>
+            <th>Phone Number</th>
+            <th>Address(City)</th>
+          </tr>
+          {personsData.map(p => (
+            <tr key={p.phoneNumber}>
+              <td data-cell="First Name">{p.firstName}</td>
+              <td data-cell="Last Name">{p.lastName}</td>
+              <td data-cell="Age">{p.age}</td>
+              <td data-cell="Married">{p.married}</td>
+              <td data-cell="Phone Number">{p.phoneNumber}</td>
+              <td data-cell="City">{p.city}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,242 @@
+export const personsData = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      age: 30,
+      married: "yes",
+      phoneNumber: "1234567890",
+      city: "New York"
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      age: 25,
+      married: "no",
+      phoneNumber: "9876543210",
+      city: "Los Angeles"
+    },
+    {
+      firstName: "David",
+      lastName: "Johnson",
+      age: 35,
+      married: "yes",
+      phoneNumber: "5678901234",
+      city: "Chicago"
+    },
+    {
+      firstName: "Sophia",
+      lastName: "Brown",
+      age: 28,
+      married: "no",
+      phoneNumber: "8765432109",
+      city: "San Francisco"
+    },
+    {
+      firstName: "Michael",
+      lastName: "Wilson",
+      age: 32,
+      married: "yes",
+      phoneNumber: "2345678901",
+      city: "Seattle"
+    },
+    {
+      firstName: "Olivia",
+      lastName: "Miller",
+      age: 27,
+      married: "no",
+      phoneNumber: "8901234567",
+      city: "Miami"
+    },
+    {
+      firstName: "Daniel",
+      lastName: "Davis",
+      age: 31,
+      married: "yes",
+      phoneNumber: "3456789012",
+      city: "Boston"
+    },
+    {
+      firstName: "Ava",
+      lastName: "Jones",
+      age: 26,
+      married: "no",
+      phoneNumber: "9012345678",
+      city: "Houston"
+    },
+    {
+      firstName: "Matthew",
+      lastName: "Taylor",
+      age: 29,
+      married: "yes",
+      phoneNumber: "6789012345",
+      city: "Denver"
+    },
+    {
+      firstName: "Isabella",
+      lastName: "Clark",
+      age: 33,
+      married: "no",
+      phoneNumber: "8901234567",
+      city: "Phoenix"
+    },
+    {
+      firstName: "Christopher",
+      lastName: "White",
+      age: 28,
+      married: "yes",
+      phoneNumber: "1234567890",
+      city: "Dallas"
+    },
+    {
+      firstName: "Mia",
+      lastName: "Wilson",
+      age: 30,
+      married: "no",
+      phoneNumber: "9876543210",
+      city: "Atlanta"
+    },
+    {
+      firstName: "Andrew",
+      lastName: "Smith",
+      age: 32,
+      married: "yes",
+      phoneNumber: "5678901234",
+      city: "Philadelphia"
+    },
+    {
+      firstName: "Emma",
+      lastName: "Johnson",
+      age: 25,
+      married: "no",
+      phoneNumber: "8765432109",
+      city: "San Diego"
+    },
+    {
+      firstName: "Ethan",
+      lastName: "Martin",
+      age: 27,
+      married: "yes",
+      phoneNumber: "2345678901",
+      city: "Portland"
+    },
+    {
+      firstName: "Olivia",
+      lastName: "Harris",
+      age: 29,
+      married: "no",
+      phoneNumber: "8901234567",
+      city: "Austin"
+    },
+    {
+      firstName: "William",
+      lastName: "Thomas",
+      age: 31,
+      married: "yes",
+      phoneNumber: "3456789012",
+      city: "Charlotte"
+    },
+    {
+      firstName: "Sophia",
+      lastName: "Lee",
+      age: 26,
+      married: "no",
+      phoneNumber: "9012345678",
+      city: "San Antonio"
+    },
+    {
+      firstName: "James",
+      lastName: "Anderson",
+      age: 30,
+      married: "yes",
+      phoneNumber: "6789012345",
+      city: "Las Vegas"
+    },
+    {
+      firstName: "Amelia",
+      lastName: "King",
+      age: 28,
+      married: "no",
+      phoneNumber: "8901234567",
+      city: "Raleigh"
+    },
+    {
+      firstName: "Benjamin",
+      lastName: "Wright",
+      age: 32,
+      married: "yes",
+      phoneNumber: "1234567890",
+      city: "Nashville"
+    },
+    {
+      firstName: "Emily",
+      lastName: "Cooper",
+      age: 27,
+      married: "no",
+      phoneNumber: "9876543210",
+      city: "Minneapolis"
+    },
+    {
+      firstName: "Lucas",
+      lastName: "Brown",
+      age: 29,
+      married: "yes",
+      phoneNumber: "5678901234",
+      city: "Orlando"
+    },
+    {
+      firstName: "Ava",
+      lastName: "Baker",
+      age: 25,
+      married: "no",
+      phoneNumber: "8765432109",
+      city: "Cincinnati"
+    },
+    {
+      firstName: "Alexander",
+      lastName: "Hill",
+      age: 33,
+      married: "yes",
+      phoneNumber: "2345678901",
+      city: "Denver"
+    },
+    {
+      firstName: "Ella",
+      lastName: "Russell",
+      age: 28,
+      married: "no",
+      phoneNumber: "8901234567",
+      city: "Tampa"
+    },
+    {
+      firstName: "Jackson",
+      lastName: "Evans",
+      age: 31,
+      married: "yes",
+      phoneNumber: "3456789012",
+      city: "Kansas City"
+    },
+    {
+      firstName: "Scarlett",
+      lastName: "Carter",
+      age: 26,
+      married: "no",
+      phoneNumber: "9012345678",
+      city: "St. Louis"
+    },
+    {
+      firstName: "Logan",
+      lastName: "Adams",
+      age: 30,
+      married: "yes",
+      phoneNumber: "6789012345",
+      city: "Detroit"
+    },
+    {
+      firstName: "Chloe",
+      lastName: "Perez",
+      age: 28,
+      married: "no",
+      phoneNumber: "8901234567",
+      city: "San Jose"
+    }
+];
